1b647f718ee352b0d50ec6d277029d6f2ae866c6
akhilesh.dk <akhileshkadam61@gmail.com>
2022-06-09 21:37:46 +0530

    Added subtract method

--- src/main/java/com/mavenDemo/App.java
+++ src/main/java/com/mavenDemo/App.java
@@ -6,6 +6,11 @@ public class App
 		return x+y;
 	}
 	
+	
+	public int subtract(int x, int y) {
+		return x-y;
+	}
+	
     public static void main( String[] args )
     {
     	System.out.println("Hello World !.");
